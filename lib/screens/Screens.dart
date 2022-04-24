eba9b0f333009547234f4c0fba25e3a72bfc87b6
JiahaoSac <Jiahaoxu.sac@gmail.com>
2022-04-24 03:35:35 -0700

    TOYT-29 Home Screen

---
+++ lib/screens/Screens.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+// import 'MessageDetailsScreen.dart';
+import 'MainScreen.dart';
+import 'MessageTabScreen.dart';
+import 'ToyDetailsScreen.dart';
+
+class MessagesScreen extends StatelessWidget{
+  const MessagesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // color: const Color(0xffC4DFCB),
+         child: MessageTabScreen(),
+      ),
+    );
+  }
+}
+
+
+class MainScreen extends StatelessWidget{
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // color: const Color(0xffC4DFCB),
+        child: MainHomeScreen(),
+      ),
+    );
+  }
+}
+
+class SearchToysScreen extends StatelessWidget{
+  const SearchToysScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // color: const Color(0xffC4DFCB),
+        child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Search Toys Screen",
+                  style: TextStyle(
+                    color: Colors.blue[900],
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+
+                const SizedBox(height: 20.0),
+                RaisedButton(
+                    child: const Text('To Toy Details'),
+                    onPressed: ()  {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const ToyDetailsScreen()));
+                    }
+                ),
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
+
+
